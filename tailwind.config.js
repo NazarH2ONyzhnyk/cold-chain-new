90d7a6cab5a595915cdd164f3559b76696dfce04
NazarH2ONyzhnyk <nazarh2onyzhnyk@gmail.com>
2022-02-13 17:41:04 +0200

    main page

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: '#F8F8F8',
+          400: '#909090',
+          800: '#181817',
+        },
+        green: {
+          400: '#8DD715',
+          500: '#70AB10',
+        },
+        blue: {
+          300: '#9CD5FF',
+          400: '#88C5F1',
+        },
+        red: {
+          300: '#EB9B8F',
+        },
+      },
+      borderRadius: {
+        large: '20px',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      scale: ['active'],
+    }
+  },
+  plugins: [],
+}
